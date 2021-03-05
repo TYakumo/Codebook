d306449e60f9f7c81a236ef367efe94236b8085d
Lin Huan Po <foxs91092@gmail.com>
2021-03-05 20:20:29 +0900

    SCC algorithm re-model

--- Tarjan (SCC).cpp
+++ Tarjan (SCC).cpp
@@ -1,66 +1,72 @@
-#include <cstdio>
-#include <vector>
-using namespace std;
+using VI = vector <int>;
+using VVI = vector <VI>;
 
-const int MAXN = 1011;
-int dfn[MAXN], low[MAXN], run[MAXN], in_stack[MAXN], st[MAXN], top, round = 1, timest, ans;
-vector <int> adja[MAXN];
+class SCCGraph {
+    VI dfn;
+    VI low;
+    VI run;
+    VI groupNum;
+    VI inStack;
+    VI stack;
+    VVI adja;
+    int round;
+    int timeStamp;
+    int curGroup;
 
-void tarjan(int now)
-{
-    dfn[now] = low[now] = ++timest;
-    run[now] = round;
-    st[top++] = now;
-    
-    for(int i = 0; i < adja[now].size(); i++)
-    {
-        int nextn = adja[now][i];
-        if(run[nextn] != round) //white
-        {
-            tarjan(nextn);
-            low[now] = low[now] < low[nextn] ? low[now] : low[nextn];
+    void __tarjan(int now) {
+        dfn[now] = low[now] = ++timeStamp;
+        run[now] = round;
+        inStack[now] = 1;
+        stack.push_back(now);
+
+        for(int i = 0; i < adja[now].size(); i++) {
+            int nextn = adja[now][i];
+            if(run[nextn] == 0) { // white
+                __tarjan(nextn);
+                low[now] = min(low[now], low[nextn]);
+            } else if(inStack[nextn]) { //gray
+                low[now] = min(low[now], dfn[nextn]);
+            }
         }
-        else if(in_stack[nextn] != round) //gray
-            low[now] = low[now] < dfn[nextn] ? low[now] : dfn[nextn];
-    }
-       
-    if(low[now] == dfn[now])
-    {
-        ans++;        
-        do{ in_stack[st[--top]] = round; }while(low[st[top]] != dfn[st[top]]);
-    }
-}
 
-int main()
-{
-    int T, N, M;
-    scanf("%d", &T);
-    
-    while(T--)
-    {
-        int f, t;
-        scanf("%d%d", &N, &M);
-        
-        for(int i = 0; i < N; i++)
-            adja[i].clear();
-                
-        while(M--)
-        {
-            scanf("%d%d", &f, &t);
-            f--, t--;
-            adja[f].push_back(t);
+        if (low[now] == dfn[now]) {
+            int top = 0;
+            do {
+                groupNum[top] = curGroup;
+                top = stack.back();
+                stack.pop_back();
+                inStack[top] = 0;
+            } while (low[top] != dfn[top]);
+            ++curGroup;
         }
-        
-        timest = ans = top = 0;        
-        for(int i = 0; i < N; i++)
-            if(run[i] != round)
-                tarjan(i);
-            
-        round++;        
-        printf("%d\n", ans);        
-        scanf("%d", &M); //clear
     }
-    
-    return 0;
-}
 
+public:
+    SCCGraph (int V) {
+        dfn = move(VI(V));
+        low = move(VI(V));
+        run = move(VI(V));
+        inStack = move(VI(V));
+        groupNum = move(VI(V, -1));
+        adja = move(VVI(V));
+        curGroup = timeStamp = round = 0;
+    }
+
+    void computeSCC() {
+        int V = dfn.size();
+        for (int i = 0; i < V; i++) {
+            if (run[i] != round) {
+                __tarjan(i);
+            }
+            ++round;
+        }
+    }
+
+    void addEdge(int f, int t) {
+        adja[f].push_back(t);
+    }
+
+    int getGroupNum(int v) {
+        return groupNum[v];
+    }
+};
